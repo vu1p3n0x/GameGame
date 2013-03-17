964f5badb28f6139cb50d307dff2e636d2932d00
Trevor Wilson <vu1p3n0x@gmail.com>
2013-03-17 01:05:51 -0500

    Removed Tests project and added Framework project

--- GameGame/WiltFramework/stdafx.cpp
+++ GameGame/WiltFramework/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// GameGameTests.pch will be the pre-compiled header
+// WiltFramework.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
--- GameGame/WiltFramework/stdafx.h
+++ GameGame/WiltFramework/stdafx.h
@@ -7,7 +7,8 @@
 
 #include "targetver.h"
 
-// Headers for CppUnitTest
-#include "CppUnitTest.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+
 
 // TODO: reference additional headers your program requires here
